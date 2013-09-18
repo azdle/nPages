609836573ef26c87e96217b2c554f417bbe3c697
Patrick Barrett <PatrickBarrett@Exosite.com>
2013-09-18 10:54:18 -0500

    Back to the features of the C++ version.

--- nPages.py
+++ nPages.py
@@ -9,48 +9,118 @@ from pyonep.alias import Alias
 class MainWindow(QMainWindow):
 
     def __init__(self, *args):
-        super(MainWindow, self).__init__()
+        super(MainWindow, self).__init__(*args)
 
-        #self.systemTrayIcon = QSystemTrayIcon(self)
-        #self.systemTrayIcon.setIcon(QIcon.fromTheme("face-smile"))
-        #self.systemTrayIcon.setVisible(True)
+        self.settings = QSettings("Exosite", "nPages")
 
-        quitButton = QPushButton("Quit")
-        vbox = QVBoxLayout()
-        vbox.addWidget(quitButton)
-        centralWidget = QWidget()
-        centralWidget.setLayout(vbox)
-        self.setCentralWidget(centralWidget)
+        ### Setup Tray Icon ###
+        self.systemTrayIcon = QSystemTrayIcon(self)
+        self.systemTrayIcon.setIcon(QIcon.fromTheme("face-smile"))
+        self.systemTrayIcon.setToolTip("nPages")
+        self.systemTrayIcon.setVisible(True)
+        self.systemTrayIcon.activated.connect(self.handleSystemTrayIconActivation)
 
-        QObject.connect(quitButton, SIGNAL("clicked()"), self, SLOT("close()"))
-        QObject.connect(quitButton, SIGNAL("clicked()"), self.closeAllPageWindows)
+        ### Create Elements ###
+        self.timeoutBar = QProgressBar()
+        self.timeoutTimer = QTimer()
 
-        cik = "0000000000000000000000000000000000000000"
+        self.cikInput = QLineEdit(self.settings.value('cik').toString())
+        self.aliasInput = QLineEdit(self.settings.value('alias').toString())
 
-        self.exoconn = Alias(cik)
-        self.settings = self.exoconn.read('sws')[0][1]
+        self.cikLabel = QLabel("CIK")
+        self.aliasLabel = QLabel("Alias")
+
+        self.saveButton = QPushButton("Save Settings")
+        self.continueButton = QPushButton("Use Existing Settings")
+
+        self.saveButtonBox = QHBoxLayout()
+        self.formLayout = QFormLayout()
+        self.formGroup = QGroupBox("Exosite Settings")
+
+        self.mainLayout = QVBoxLayout()
+        self.centralWidget = QWidget()
+
+        ### Setup Elements ###
+        self.cikInput.setMinimumSize(QSize(325, 0))
+
+        self.timeoutBar.setTextVisible(False)
+        self.timeoutBar.setMinimum(0)
+        self.timeoutBar.setMaximum(100)
+        self.timeoutBar.setInvertedAppearance(True)
+        self.timeoutBar.setValue(100)
+
+        self.timeoutTimer.timeout.connect(self.WindowTimerTick)
+        self.timeoutTimer.start(100)
+        self.countdown = 100
+
+        self.continueButton.clicked.connect(self.Launch)
+        self.saveButton.clicked.connect(self.saveSettingsThenLaunch)
+
+        ### Layout Elements ###
+        self.formLayout.addRow(self.cikLabel, self.cikInput)
+        self.formLayout.addRow(self.aliasLabel, self.aliasInput)
+        self.formGroup.setLayout(self.formLayout)
+
+        self.saveButtonBox.addWidget(self.continueButton)
+        self.saveButtonBox.addWidget(self.saveButton)
+
+        self.mainLayout.addWidget(self.formGroup)
+        self.mainLayout.addItem(self.saveButtonBox)
+        self.mainLayout.addWidget(self.timeoutBar)
+
+        self.centralWidget.setLayout(self.mainLayout)
+        self.setCentralWidget(self.centralWidget)
 
         self.PageWindows = []
 
-        print(self.settings)
+        self.show()
 
-        self.generatePageWindows(json.loads(self.settings))
-
-        #self.systemTrayIcon.showMessage('Running', 'Running in the background.')
+    def Launch(self):
         self.hide()
+        self.generatePageWindows()
 
-    def generatePageWindows(self, settings):
+        self.timeoutTimer.stop()
+        self.timeoutBar.hide()
+
+    def WindowTimerTick(self):
+        if self.cikInput.text().length() != 40 or self.aliasInput.text().length() == 0:
+            self.countdown = 100
+        elif self.countdown <= 0:
+            self.Launch()
+        else:
+            self.countdown = self.countdown - 1
+
+        self.timeoutBar.setValue(self.countdown)
+
+    def generatePageWindows(self):
         self.closeAllPageWindows()
-        for this_window in settings:
+        for this_window in self.fetchPageSettings():
             self.PageWindows.append(PageWindow(this_window))
 
     def closeAllPageWindows(self):
-        #This may be slightly hacky, but garbage collector will delete all PageWindows
         self.PageWindows = []
+
+    def handleSystemTrayIconActivation(self, reason):
+        if reason == QSystemTrayIcon.Context:
+            pass
+        elif reason == QSystemTrayIcon.Trigger:
+            self.setVisible(not self.isVisible())
+
+    def fetchPageSettings(self):
+        self.exoconn = Alias(str(self.settings.value('cik').toString()))
+        settingsString = self.exoconn.read(str(self.settings.value('alias').toString()))[0][1]
+        print settingsString
+        return json.loads(settingsString)
+
+    def saveSettingsThenLaunch(self):
+        self.settings.setValue('cik', self.cikInput.text())
+        self.settings.setValue('alias', self.aliasInput.text())
+
+        self.Launch()
 
 class PageWindow(QWebView):
     def __init__(self, settings, *args):
-        super(PageWindow, self).__init__()
+        super(PageWindow, self).__init__(*args)
 
         self.settings = settings
         self.pageIndex = 0
@@ -59,8 +129,7 @@ class PageWindow(QWebView):
         self.showFullScreen()
 
         self.pageTimer = QTimer()
-        QObject.connect(self.pageTimer, SIGNAL("timeout()"),
-                        self.nextPage)
+        self.pageTimer.timeout.connect(self.nextPage)
         self.pageTimer.start(self.settings['interval'])
 
     def nextPage(self, url = None):
@@ -75,10 +144,7 @@ class PageWindow(QWebView):
 
 def main(args):
     app = QApplication(args)
-    #pageView = PageWindow()
-    #pageView.show()
     win = MainWindow()
-    win.show()
     app.connect(app, SIGNAL("lastWindowClosed()"),
                 app, SLOT("quit()"))
     app.exec_()
